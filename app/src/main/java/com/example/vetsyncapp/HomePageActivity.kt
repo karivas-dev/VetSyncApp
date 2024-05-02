78739d444c56adb9dbb25fda833f693fdfe95502
karivas-dev <karenrivasg17@gmail.com>
2024-05-01 18:11:24 -0600

    Navigation Fix

--- app/src/main/java/com/example/vetsyncapp/HomePageActivity.kt
+++ app/src/main/java/com/example/vetsyncapp/HomePageActivity.kt
@@ -1,28 +1,23 @@
 package com.example.vetsyncapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Toast
-
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.widget.AppCompatButton
-import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
-import com.google.android.material.navigation.NavigationView
-import com.google.firebase.auth.FirebaseAuth
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.vetsyncapp.databinding.FragmentHomeBinding
 
 
 class HomePageActivity : AppCompatActivity() {
-    private lateinit var btnLogout: AppCompatButton
+    private val binding by lazy {
+        FragmentHomeBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_home)
+        setContentView(binding.root)
 
         val navController = findNavController(R.id.mainHostFragment)
+        binding.btnNav.setupWithNavController(navController)
     }
 }
